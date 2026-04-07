Fix P1 regressions in loss masking and inference/com loaders

dannce/engine/data/processing.py
@@ -260,6 +260,8 @@ def load_all_com_exps(params: Dict, exps: List):
     camnames = {}
     datadict = {}
     datadict_3d = {}
+    com3d_dict = {}
+    temporal_chunks = {}
     samples = []
     for e, expdict in enumerate(exps):
 
@@ -298,7 +300,16 @@ def load_all_com_exps(params: Dict, exps: List):
 
     samples = np.array(samples)
 
-    return samples, datadict, datadict_3d, com3d_dict, cameras, camnames, total_chunks, temporal_chunks
+    return (
+        samples,
+        datadict,
+        datadict_3d,
+        com3d_dict,
+        cameras,
+        camnames,
+        total_chunks,
+        temporal_chunks,
+    )
 
 
 def do_COM_load(exp: Dict, expdict: Dict, e, params: Dict, training=True):

dannce/engine/models/loss.py
@@ -33,9 +33,7 @@ def compute_mask_nan_loss(loss_fcn, kpts_gt, kpts_pred):
     valid_gt = kpts_gt[notnan_gt]
     valid_pred = kpts_pred[notnan_gt]
     
-    # Compute loss using mean reduction for better gradient flow
-    loss = torch.nn.functional.l1_loss(valid_pred, valid_gt, reduction='mean')
-    return loss
+    return loss_fcn(valid_gt, valid_pred)
 
 
 ##################################################################################################